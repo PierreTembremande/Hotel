ceea5ec5493d010ba26bc3cf3441bf3f1dcd11bc
PierreTembremande <pierre.tembremande@gmail.com>
2021-10-21 10:02:28 +0200

    Projet Hotel

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'Pio',
+        ]);
+    }
+
+    /**
+     * @Route("/chevalier/{id<\d+>}", name="chevalier")
+     */
+    public function test($id=1): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => $id,
+        ]);
+    }
+}
